24e7367ebf750c1f27591beabe3a357ae443bf13
lucagad <lucagaddini@icloud.com>
2022-06-08 16:09:34 +0200

    {PHP} Added: page with dynamic contents coming from an array of objects

---
+++ index.php
@@ -0,0 +1,222 @@
+<?php
+
+
+$db = [
+  [
+    'title' => 'Come state implementando la recente decisione della Corte di giustizia dell&#39 Unione europea (CGUE) relativa al diritto all&#39oblio?',
+    'text' => '<p>
+    La recente <a href="http://curia.europa.eu/juris/document/document.jsf?text=&docid=152065&amppageIndex=0&doclang=it&mode=lst&dir=&occ=first&part=1&cid=276332"> decisione della Corte di giustizia dell&#39Unione europea </a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+    </p>
+    <p>
+    Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell&#39individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
+    </p>
+    <p>
+    Per presentare una richiesta di rimozione, compila questo <a href="https://support.google.com/legal/contact/lr_eudpa?product=websearch&hl=it">modulo web</a> . Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po&#39 di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d&#39accordo con la nostra valutazione, puoi rivolgerti all&#39Autorità garante per la protezione dei dati personali nel tuo paese.
+    </p>
+    <p>
+    Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
+    </p>
+    <p>
+    Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.
+    </p>'
+  ],
+  [
+    'title' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
+    'text' => '<p>
+    Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un&#39elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+    </p>
+    <p>
+    Siamo costantemente al lavoro per garantire un&#39elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
+    </p>
+    <p>
+    Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href="https://www.google.com/safetycenter/">Centro Google per la sicurezza online</a>.
+    </p>
+    <p>
+    <a href="https://privacy.google.com/">Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.
+    </p>'
+  ],
+  [
+    'title' => 'Perché il mio account è associato a un paese?',
+    'text' => '<p>
+    Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+    </p>
+    <ul>
+      <li>
+      La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+      <ul>
+      <li>
+      Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell&#39Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+      </li>
+      <li>
+      Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+      </li>
+      </ul>
+      </li>
+      <li>
+      La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+      </li>
+    </ul>
+    
+    <p>
+    Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
+    </p>
+    
+    <h5 class="my-3">Stabilire il paese associato al tuo account</h5>
+    
+    <p>
+    Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell&#39ultimo anno.
+    </p>
+    <p>
+    I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l&#39associazione del paese.
+    </p>
+    <p>
+    Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l&#39installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href="https://policies.google.com/country-association-contact-us">Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.
+    </p>'
+  ],
+  [
+    'title' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
+    'text' => '<p>
+    I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href="https://support.google.com/websearch/answer/9109?hl=it">contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href="https://support.google.com/legal/contact/lr_eudpa?product=websearch&hl=it">fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l&#39aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href="https://support.google.com/websearch/troubleshooter/3111061?hl=it">visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.
+    </p>'
+  ],
+  [
+    'title' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
+    'text' => '<p>
+    In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l&#39indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href="https://policies.google.com/privacy/key-terms#toc-terms-referrer-url">URL referrer</a>. Talvolta, l&#39URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell&#39URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href="https://support.google.com/websearch/answer/173733">qui</a>. Le query di ricerca o le informazioni contenute nell&#39URL referrer potrebbero essere disponibili mediante Google Analytics o un&#39API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all&#39 esatte parole chiave che hanno determinato il clic su un annuncio.
+    </p>'
+  ]
+];
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="./assets/style/style.css">
+
+  <title>Domande frequenti | Privacy e termini | Google</title>
+</head>
+<body>
+
+  <header>
+    <section class="py-3 border-bottom">
+        <div class="container-fluid d-flex flex-wrap justify-content-center">
+          <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+            <img class="bi me-2" width="30%" height="25px" src="./assets/img/logo.png" alt="Logo">  
+            <span class="fs-5">Privacy e termini</span>
+          </a>
+          <div class="dropdown text-end">
+            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+            </a>
+            <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Sign out</a></li>
+            </ul>
+          </div>
+        </div>
+    </section>
+
+    <nav class="py-2 bg-light border-bottom">
+        <div class="container-fluid d-flex flex-wrap">
+          <ul class="nav me-auto">
+            <li class="nav-item"><a href="#" class="nav-link link-dark px-2" aria-current="page">Introduzione</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Norme sulla privacy</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Termini di servizio</a></li>
+            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Tecnologie</a></li>
+            <li class="nav-item active"><a href="#" class="nav-link link-dark px-2 active">Domande frequenti</a></li>
+          </ul>
+        </div>
+      </nav>
+  </header>
+
+  <main>
+    <div class="container">
+      <div class="faqs_container m-5">
+        <?php foreach ($db as $faqs => $faq): ?>
+          
+          <h4 class="my-5"> <?php echo $faq['title']; ?> </h4>
+          <?php echo $faq['text']; ?> 
+        <?php endforeach; ?>
+      </div>
+    </div>
+
+  </main>
+
+  <footer>
+    <div class="footer_custom">
+      <div class="footer_container">
+        
+        <ul class="ul_custom">
+          <li class="li_custom mx-2">
+            <a href="https://www.google.com/" class="a_custom">Google</a>
+          </li>
+          <li class="li_custom mx-2">
+            <a href="https://about.google/" class="a_custom">Tutto su Google</a>
+          </li>
+          <li class="li_custom mx-2">
+            <a href="privacy" class="a_custom">Privacy</a>
+          </li>
+          <li class="li_custom mx-2">
+            <a href="terms" class="a_custom">Termini</a>
+          </li>
+        </ul>
+        
+        <div class="flag">
+          <label class="gZggid" for="i3">Cambia lingua:</label>
+          <select class="iCthae" id="i3" jsname="O1htCb">
+            <option value="af">Afrikaans</option>
+            <option value="id">Bahasa Indonesia</option>
+            <option value="ms">Bahasa Melayu</option>
+            <option value="ca">Català</option>
+            <option value="cs">Čeština</option>
+            <option value="da">Dansk</option>
+            <option value="de">Deutsch</option>
+            <option value="et">Eesti</option>
+            <option value="en">English</option>
+            <option value="en-GB">English (United Kingdom)</option>
+            <option value="es">Español</option>
+            <option value="es-419">Español (Latinoamérica)</option>
+            <option value="eu">Euskara</option>
+            <option value="fil">Filipino</option>
+            <option value="fr">Français</option>
+            <option value="fr-CA">Français (Canada)</option>
+            <option value="gl">Galego</option>
+            <option value="hr">Hrvatski</option>
+            <option value="zu">Isizulu</option>
+            <option value="is">Íslenska</option>
+            <option selected="" value="it">Italiano</option>
+            <option value="sw">Kiswahili</option>
+            <option value="lv">Latviešu</option>
+            <option value="lt">Lietuvių</option>
+            <option value="hu">Magyar</option>
+            <option value="nl">Nederlands</option>
+            <option value="no">Norsk</option>
+            <option value="pl">Polski</option>
+            <option value="pt-BR">Português (Brasil)</option>
+            <option value="pt-PT">Português (Portugal)</option>
+            <option value="ro">Română</option>
+            <option value="sk">Slovenčina</option>
+            <option value="sl">Slovenščina</option>
+            <option value="sr-Latn">Srpski</option>
+            <option value="fi">Suomi</option>
+            <option value="sv">Svenska</option>
+          </select>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+</body>
+</html>
